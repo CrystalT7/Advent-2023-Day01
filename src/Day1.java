a35c5756bea583c1b9de872d0c081301c8d3437b
CrystalTsang <sample@bths.edu>
2025-11-24 10:18:06 -0500

    day 2

--- src/Day1.java
+++ src/Day1.java
@@ -1,3 +1,4 @@
+/*
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -5,7 +6,7 @@ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Day1 {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -89,4 +90,4 @@ public class Main {
             return fileData;
         }
     }
-}
+}*/
